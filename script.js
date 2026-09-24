38a8a91c7bb277f61f2f17b8c0fc081d1e1cc6ed
xboxman234 <mrtylersolid@gmail.com>
2026-09-24 18:09:22 -0500

    added servings

--- script.js
+++ script.js
@@ -17,8 +17,7 @@ async function getMenu(market, meal) {
         const response = await fetch(link);
         if (!response.ok) {
             alert("Something lowk went wrong");
-            document.getElementById("meal").disabled = false;
-    document.getElementById("field").disabled = false;
+
             return;
         }
 
@@ -39,8 +38,7 @@ async function getItems(id) {
         const response = await fetch(link);
         if (!response.ok) {
             alert("Something lowk went wrong");
-            document.getElementById("meal").disabled = false;
-    document.getElementById("field").disabled = false;
+
             return;
         }
 
@@ -75,73 +73,82 @@ async function doEverything() {
     document.getElementById("meal").disabled = true;
     document.getElementById("field").disabled = true;
 
-    
-        document.querySelectorAll(".Item").forEach(element => {
-            element.remove();
 
-        });
+    document.querySelectorAll(".Item").forEach(element => {
+        element.remove();
 
-    
-
-    // fetch the menus from all checked markets at once and return when they get returned (meal comes from dropdown)
-    var fetchdays = locations.map(async (item) => {
-        const response = await getMenu(item, document.getElementById("meal").value);
-        return await response;
     });
-    var alldays = await Promise.all(fetchdays)
 
-    // Loop through each market return menu
-    for (var week = 0; week < alldays.length; week++) {
+    try {
 
-        // ids for subset foods like yogurt bar
+        // fetch the menus from all checked markets at once and return when they get returned (meal comes from dropdown)
+        var fetchdays = locations.map(async (item) => {
+            const response = await getMenu(item, document.getElementById("meal").value);
+            return await response;
+        });
+        var alldays = await Promise.all(fetchdays)
+        // list of ids per day
         let ids = [];
-        console.log("location" + locations[week]);
+        // Loop through each market return menu
+        for (var week = 0; week < alldays.length; week++) {
+            document.getElementById("loading").textContent += ".";
 
-        // loop through each day of the week to see if its today (probaly a better way to do this)
-        for (var i of alldays[week]["days"]) {
-            console.log(new Date().toLocaleDateString('sv-SE'))
-            if (i["date"] === new Date().toLocaleDateString('sv-SE')) {
-                day = i;
-            }
-        }
-        console.log(day)
+            // ids for subset foods like yogurt bar
 
-        // loop through every item of the list of foods of the day of the week
-        for (var i of day["menu_items"]) {
-            // skip useless values
-            if (i["food"] == null || i["food"]["is_section_title"]) {
-                continue;
-            }
+            console.log("location" + locations[week]);
 
-            // push nested foods ids to ids to get later
-            // each entry is the id and the object
-            if (i["food"]["nested_foods"].length != 0) {
-                ids.push([i["id"], i["food"]["nested_foods"]]);
-                console.log(i["id"], i["food"]["nested_foods"])
-                continue
-            }
-
-            // target lowk returns the object with the same name as the one we are looking at
-            // if it exists, it just adds the market if it doesnt it makes a new entry
-            var target = Object.values(menu).find(user => user.name === i["food"]["name"]);
-            if (target) {
-                if (!target.locations.includes(NAMES[locations[week]])) {
-                    target.locations.push(NAMES[locations[week]]);
+            // loop through each day of the week to see if its today (probaly a better way to do this)
+            for (var i of alldays[week]["days"]) {
+                console.log(new Date().toLocaleDateString('sv-SE'))
+                if (i["date"] === new Date().toLocaleDateString('sv-SE')) {
+                    day = i;
                 }
             }
-            else {
-                // menu item
-                menu.push({
+            console.log(day)
 
-                    "name": i["food"]["name"],
-                    "protien": i["food"]["rounded_nutrition_info"]["g_protein"],
-                    "calories": i["food"]["rounded_nutrition_info"]["calories"],
-                    "ratio":
-                        parseFloat(i["food"]["rounded_nutrition_info"]["g_protein"]) / parseFloat(i["food"]["rounded_nutrition_info"]["calories"]),
-                    "locations": [NAMES[locations[week]]]
-                })
+            // loop through every item of the list of foods of the day of the week
+            for (var i of day["menu_items"]) {
+                // skip useless values
+                if (i["food"] == null || i["food"]["is_section_title"]) {
+                    continue;
+                }
+
+                // push nested foods ids to ids to get later
+                // each entry is the id and the object
+
+
+                if (i["food"]["nested_foods"].length != 0) {
+                    ids.push([i["id"], i["food"]["nested_foods"], NAMES[locations[week]]]);
+                    console.log(i["id"], i["food"]["nested_foods"])
+                    continue
+                }
+
+
+
+                // target lowk returns the object with the same name as the one we are looking at
+                // if it exists, it just adds the market if it doesnt it makes a new entry
+                var target = Object.values(menu).find(user => user.name === i["food"]["name"]);
+                if (target) {
+                    if (!target.locations.includes(NAMES[locations[week]])) {
+                        target.locations.push(NAMES[locations[week]]);
+                    }
+                }
+                else {
+                    // menu item
+                    menu.push({
+
+                        "name": i["food"]["name"],
+                        "protien": i["food"]["rounded_nutrition_info"]["g_protein"],
+                        "calories": i["food"]["rounded_nutrition_info"]["calories"],
+                        "ratio":
+                            parseFloat(i["food"]["rounded_nutrition_info"]["g_protein"]) / parseFloat(i["food"]["rounded_nutrition_info"]["calories"]),
+                        "locations": [NAMES[locations[week]]],
+                        "serving": i["food"]["serving_size_info"]["serving_size_amount"]+" servings, "+i["food"]["serving_size_info"]["serving_size_unit"]
+                    })
+                }
             }
         }
+
         // the list of ids returned from the last call
         let fetchids = ids.map(async (item) => {
             // for some reason the location is the same for all markets
@@ -154,6 +161,8 @@ async function doEverything() {
 
         // loop through the retuned id nutrition info
         for (var i = 0; i < ids.length; i++) {
+            
+
             let items = idsResponses[i];
             console.log("-----")
             console.log(items)
@@ -161,14 +170,18 @@ async function doEverything() {
 
             // forgot why i need to loop through here again
             for (var l of ids[i][1]) {
+                // supposed to give an indication of how its loading, lowk doesnt work at all idk why
+                document.getElementById("loading").textContent += ".";
                 // thing is the parent of the food
                 var thing = items.nested_option_container_map[l]
                 console.log(thing)
 
                 // see if the name is already in menu
                 var target = Object.values(menu).find(user => user.name === thing["name"]);
-                if (target && !target.locations.includes(NAMES[locations[week]])) {
-                    target.locations.push(NAMES[locations[week]]);
+                if (target) {
+                    if (!target.locations.includes(ids[i][2])) {
+                        target.locations.push(ids[i][2]);
+                    }
 
                 } else {
                     menu.push({
@@ -177,16 +190,22 @@ async function doEverything() {
                         "calories": thing["rounded_nutrition_info"]["calories"],
                         "ratio":
                             parseFloat(thing["rounded_nutrition_info"]["g_protein"]) / parseFloat(thing["rounded_nutrition_info"]["calories"])
-                        , "locations": [NAMES[locations[week]]]
+                        , "locations": [ids[i][2]],
+                        "serving": thing["serving_size_info"]["serving_size_amount"]+" servings, "+thing["serving_size_info"]["serving_size_unit"]
                     })
                 }
 
             }
         }
 
-
+    } catch {
+        alert("Something pertaining to the api data went wrong")
+    } finally {
+        document.getElementById("meal").disabled = false;
+        document.getElementById("field").disabled = false;
     }
-    
+
+
     // sort by ratio (or 0 if its undefined or somthing)
     menu.sort((a, b) => (b.ratio || 0) - (a.ratio || 0))
     document.getElementById("loading").textContent = "";
@@ -200,7 +219,7 @@ async function doEverything() {
         //"Calories" + i.calories + "Protien:" + i.protien + " " + i.name +i.week
         item.appendChild(
             Object.assign(document.createElement("p"),
-                { textContent: i.locations.join(", ") + ": " + i.name + " " + i.protien + " grams of protien " + i.calories + " calories." }))
+                { textContent: i.locations.join(", ") + ": " + i.name + " " + i.protien + " grams of protien " + i.calories + " calories for " + i.serving}))
         document.body.appendChild(item);
 
     }
