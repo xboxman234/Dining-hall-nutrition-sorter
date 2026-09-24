1f24d951cde13cbf1faa893816538d7aae749098
xboxman234 <mrtylersolid@gmail.com>
2026-09-24 01:40:25 -0500

    lots of changes

--- script.js
+++ script.js
@@ -1,41 +1,17 @@
-// menu https://wisc-housingdining.nutrislice.com/menu
 
-//ToDO:
-// Breakfast Lunch and Dinner checkboxes
-// Market Checkboxes
-//
-var week;
-var day;
-var menu = [];
-var locations = [
-    "four-lakes-market",
-    "carsons-market",
-    "gordon-avenue-market",
-    "lizs-market",
-    "rhetas-market"
+// Api name : display name
+const NAMES = {
+    "four-lakes-market": "Four Lakes",
+    "carsons-market": "Carson's",
+    "gordon-avenue-market": "Gordon Avenue",
+    "lizs-market": "Liz's",
+    "rhetas-market": "Rheta's"
+};
 
-
-];
+// gets the menu for the week from nutrislice
 async function getMenu(market, meal) {
-    //change date later
-    const link = "https://worker.mrtylersolid.workers.dev/?market=" + market + "&meal=" + meal;
-    console.log(link);
-    try {
-        const response = await fetch(link);
-        if (!response.ok) {
-            alert("bad");
-            return;
-        }
-
-        return await response.json();
-    } catch (error) {
-        console.error('Failed to fetch or parse Nutrislice menu:', error);
-    }
-    return 1;
-}
-async function getItems(id, loc) {
-    //change date later
-    const link = "https://worker.mrtylersolid.workers.dev/?type=multi&id=" + id + "&loc=" + loc;
+    let date = new Date().toLocaleDateString('sv-SE').replaceAll("-","/");
+    const link = "https://worker.mrtylersolid.workers.dev/?market=" + market + "&meal=" + meal + "&date=" + date;
     console.log(link);
     try {
         const response = await fetch(link);
@@ -51,40 +27,63 @@ async function getItems(id, loc) {
     return 1;
 }
 
+// get a list of items (provided by getMenu) and return their properties
+async function getItems(id) {
+    //change date later
+    let date = new Date().toLocaleDateString('sv-SE');
+    const link = "https://worker.mrtylersolid.workers.dev/?type=multi&id=" + id + "&date=" + date;
+    console.log(link);
+    try {
+        const response = await fetch(link);
+        if (!response.ok) {
+            alert("bad");
+            return;
+        }
+
+        return await response.json();
+    } catch (error) {
+        console.error('Failed to fetch or parse Nutrislice menu:', error);
+    }
+    return 1;
+}
+
+// do everything
 async function doEverything() {
+    // week of menus for a market
+    var week;
 
-    locations = [
+    // current day in terms of the returned menu
+    var day = null;
 
-    ];
+    // eventual menu to put to the screen
+    var menu = [];
+
+    //Which markets are used for menu decided by checkboxes
+    var locations = [];
     document.getElementById("four-lakes-market").checked && locations.push(document.getElementById("four-lakes-market").value)
     document.getElementById("carsons-market").checked && locations.push(document.getElementById("carsons-market").value)
     document.getElementById("gordon-avenue-market").checked && locations.push(document.getElementById("gordon-avenue-market").value)
     document.getElementById("lizs-market").checked && locations.push(document.getElementById("lizs-market").value)
     document.getElementById("rhetas-market").checked && locations.push(document.getElementById("rhetas-market").value)
 
+    // loading bar to show that things are in fact working
     document.getElementById("loading").textContent = "Loading...";
+
+    // fetch the menus from all checked markets at once and return when they get returned (meal comes from dropdown)
     var fetchdays = locations.map(async (item) => {
         const response = await getMenu(item, document.getElementById("meal").value);
         return await response;
     });
-    alldays = await Promise.all(fetchdays)
+    var alldays = await Promise.all(fetchdays)
 
-
-
-    if (document.getElementsByClassName("Item")) {
-        document.querySelectorAll(".Item").forEach(element => {
-            element.remove();
-
-        });
-    }
-    menu = [];
-    console.log(alldays)
+    // Loop through each market return menu
     for (var week = 0; week < alldays.length; week++) {
-        //week = await getMenu("four-lakes-market", "dinner")
-        ids = [];
+
+        // ids for subset foods like yogurt bar
+        let ids = [];
         console.log("location" + locations[week]);
 
-
+        // loop through each day of the week to see if its today (probaly a better way to do this)
         for (var i of alldays[week]["days"]) {
             console.log(new Date().toLocaleDateString('sv-SE'))
             if (i["date"] === new Date().toLocaleDateString('sv-SE')) {
@@ -93,55 +92,76 @@ async function doEverything() {
         }
         console.log(day)
 
+        // loop through every item of the list of foods of the day of the week
         for (var i of day["menu_items"]) {
+            // skip useless values
             if (i["food"] == null || i["food"]["is_section_title"]) {
                 continue;
             }
+
+            // push nested foods ids to ids to get later
             if (i["food"]["nested_foods"].length != 0) {
                 ids.push([i["id"], i["food"]["nested_foods"]]);
                 console.log(i["id"], i["food"]["nested_foods"])
                 continue
             }
-            const targetUser = Object.values(menu).find(user => user.name === i["food"]["name"]);
-            menu.push({
 
-                "name": i["food"]["name"],
-                "protien": i["food"]["rounded_nutrition_info"]["g_protein"],
-                "calories": i["food"]["rounded_nutrition_info"]["calories"],
-                "ratio":
-                    parseFloat(i["food"]["rounded_nutrition_info"]["g_protein"]) / parseFloat(i["food"]["rounded_nutrition_info"]["calories"]),
-                "week": locations[week]
-            })
+            
+            var target = Object.values(menu).find(user => user.name === i["food"]["name"]);
+            if (target && !target.locations.includes(NAMES[locations[week]])) {
+                target.locations.push(NAMES[locations[week]]);
+            }
+            else {
+                menu.push({
+
+                    "name": i["food"]["name"],
+                    "protien": i["food"]["rounded_nutrition_info"]["g_protein"],
+                    "calories": i["food"]["rounded_nutrition_info"]["calories"],
+                    "ratio":
+                        parseFloat(i["food"]["rounded_nutrition_info"]["g_protein"]) / parseFloat(i["food"]["rounded_nutrition_info"]["calories"]),
+                    "locations": [NAMES[locations[week]]]
+                })
+            }
         }
         let fetchids = ids.map(async (item) => {
             let response = await getItems(item[0], 45371);
             return await response;
         });
-        idsResponses = await Promise.all(fetchids)
+        var idsResponses = await Promise.all(fetchids)
         console.log("ids", ids)
         for (var i = 0; i < ids.length; i++) {
-            items = idsResponses[i]
+            let items = idsResponses[i];
             console.log("-----")
             console.log(items)
             console.log("nested", items["nested_option_container_map"])
             for (var l of ids[i][1]) {
-                thing = items.nested_option_container_map[l]
+                var thing = items.nested_option_container_map[l]
                 console.log(thing)
+                var target = Object.values(menu).find(user => user.name === thing["name"]);
+                if (target && !target.locations.includes(NAMES[locations[week]])) {
+                    target.locations.push(NAMES[locations[week]]);
 
-                menu.push({
-                    "name": thing["name"],
-                    "protien": thing["rounded_nutrition_info"]["g_protein"],
-                    "calories": thing["rounded_nutrition_info"]["calories"],
-                    "ratio":
-                        parseFloat(thing["rounded_nutrition_info"]["g_protein"]) / parseFloat(thing["rounded_nutrition_info"]["calories"])
-                    , "week": locations[week]
-                })
+                } else {
+                    menu.push({
+                        "name": thing["name"],
+                        "protien": thing["rounded_nutrition_info"]["g_protein"],
+                        "calories": thing["rounded_nutrition_info"]["calories"],
+                        "ratio":
+                            parseFloat(thing["rounded_nutrition_info"]["g_protein"]) / parseFloat(thing["rounded_nutrition_info"]["calories"])
+                        , "locations": [NAMES[locations[week]]]
+                    })
+                }
 
             }
         }
 
 
-    } menu.sort((a, b) => (b.ratio || 0) - (a.ratio || 0))
+    } if (document.getElementsByClassName("Item")) {
+        document.querySelectorAll(".Item").forEach(element => {
+            element.remove();
+
+        });
+    }menu.sort((a, b) => (b.ratio || 0) - (a.ratio || 0))
     document.getElementById("loading").textContent = ""
     for (i of menu) {
         const item = document.createElement("div");
@@ -151,7 +171,7 @@ async function doEverything() {
         //"Calories" + i.calories + "Protien:" + i.protien + " " + i.name +i.week
         item.appendChild(
             Object.assign(document.createElement("p"),
-                { textContent: "Market:" + i.week + " " + i.name + " " + i.protien + " grams of protien " + i.calories + " calories." + " remove later_" + i.ratio }))
+                { textContent: i.locations.join(", ") + ": " + i.name + " " + i.protien + " grams of protien " + i.calories + " calories." }))
         document.body.appendChild(item);
 
     }
